fc205edf7046af161623329199ac0c167fda4906
Vikram Sreedhar <vikramsreedhar@DN51vc1q.SUNet>
2015-10-29 03:12:13 -0700

    new fixed

--- script.js
+++ script.js
@@ -5,29 +5,45 @@ $(document).ready(function(){
 	//applyPreloader();
 	//randomFeature();
 
-$('.nav .dropdown').hover(function() {
-        $('#menu1').css('display', 'block');
-    }, function() {
-        $('#menu1').css('display', 'none');
-    });
+//DYNAMIC SUBMENU CLASS SYMBOLS
+// $("#navbarMenu ul li").has( "ul" ).each(function( index ) {
+//     $(this).toggleClass('submenu');
+// });
+$("#navbarMenu ul .submenu > a").append('<span class="submenu-button"></span>');
 
-$('.nav .dropdown1').hover(function() {
-        $('#menu2').css('display', 'block');
-    }, function() {
-        $('#menu2').css('display', 'none');
-    });
+//OPEN TOGGLE MENU ON BREAKPOINT MEDIA QUERY
+$(".toggleMenu").click(function(e){
+    $("nav > ul").slideToggle();
+    e.preventDefault();
+});
 
-$('.nav .dropdown2').hover(function() {
-        $('#menu3').css('display', 'block');
-    }, function() {
-        $('#menu3').css('display', 'none');
-    });
+//TO PREVENT MENU CLOSE ON CLICK IN SUBMENU
+$(".submenu ul li").click(function(e){
+    e.stopPropagation();
+});
 
-$('.nav .dropdown3').hover(function() {
-        $('#menu4').css('display', 'block');
-    }, function() {
-        $('#menu4').css('display', 'none');
-    });
+//CLICK ON SUBMENU LINK
+$(".submenu").click(function(e){
+    //CLOSE OPEN SUBMENU
+    if($(this).hasClass("active")){
+        $("#navbarMenu ul li").removeClass('active');
+        $('#navbarMenu ul li ul').slideUp();
+    }else{
+        $("#navbarMenu ul li").removeClass('active');
+        $("#navbarMenu ul li ul").slideUp();
+        $(this).addClass("active");
+        $(this).find('ul').stop().slideToggle();
+        e.stopPropagation();
+    }
+    $("#navbarMenu ul li").removeClass('openSub');
+    $(this).addClass('openSub');
+
+});
+
+$("body, html").click(function(){
+    $("#navbarMenu ul li").removeClass('active openSub');
+    $('#navbarMenu ul li ul').slideUp();
+});
 
 
 	applyHeader();
