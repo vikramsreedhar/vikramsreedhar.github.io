306c575392ecd01972279a1c73c4865dc66fb1db
Vikram Sreedhar <vikramsreedhar@DN51vc1q.SUNet>
2015-10-29 03:22:39 -0700

    new fixed

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ $(document).ready(function(){
 	//applyPreloader();
 	//randomFeature();
 
+$('.nav .dropdown').hover(function() {
+        $('#menu1').css('display', 'block');
+    }, function() {
+        $('#menu1').css('display', 'none');
+    });
+
+$('.nav .dropdown1').hover(function() {
+        $('#menu2').css('display', 'block');
+    }, function() {
+        $('#menu2').css('display', 'none');
+    });
+
+$('.nav .dropdown2').hover(function() {
+        $('#menu3').css('display', 'block');
+    }, function() {
+        $('#menu3').css('display', 'none');
+    });
+
+$('.nav .dropdown3').hover(function() {
+        $('#menu4').css('display', 'block');
+    }, function() {
+        $('#menu4').css('display', 'none');
+    });
+
 
 	applyHeader();
 	applyNavigation(); 
